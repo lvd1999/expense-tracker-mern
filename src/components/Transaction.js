6178933cbd4a41bac05be3ee9f77a0c9b5c4fb6c
Patrick <patricklee0221@gmail.com>
2021-10-16 11:13:03 +0100

    dynamic styling

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,10 +4,16 @@ import { GlobalContext } from '../context/GlobalState';
 export const Transaction = ({ transaction }) => {
   const { transactions } = useContext(GlobalContext);
 
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
   return (
     <div>
-      <li className="minus">
-        {transactions.text} <span>-$400</span>
+      <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+        {transactions.text}{' '}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
         <button className="delete-btn">x</button>
       </li>
     </div>
