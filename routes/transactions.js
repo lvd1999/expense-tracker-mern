f7b72782a30725a895439e34fc4ce4c5e0ba10b8
Patrick <patricklee0221@gmail.com>
2021-10-21 12:01:44 +0100

    post and delete routers

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions } = require('../controllers/transactions');
+const {
+  getTransactions,
+  addTransactions,
+  deleteTransactions,
+} = require('../controllers/transactions');
 
-router.route('/').get(getTransactions);
+router.route('/').get(getTransactions).post(addTransactions);
+
+router.route('/:id').delete(deleteTransactions);
 
 module.exports = router;
