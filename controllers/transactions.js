f40aa9fbef0232b42cf1d04ade0329a3b502f5d5
Patrick <patricklee0221@gmail.com>
2021-10-22 10:43:04 +0100

    add transaction response

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -13,7 +13,7 @@ exports.getTransactions = async (req, res, next) => {
       data: transactions,
     });
   } catch (error) {
-    return res.send(500).json({
+    return res.status(500).json({
       success: false,
       error: 'Server error',
     });
@@ -24,7 +24,30 @@ exports.getTransactions = async (req, res, next) => {
 // @route   POST /api/v1/transactions
 // @access  Public
 exports.addTransactions = async (req, res, next) => {
-  res.send('POST transactions');
+  try {
+    const { text, amount } = req.body;
+
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      const messages = Object.values(error.errors).map((val) => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server error',
+      });
+    }
+  }
 };
 
 // @desc    Delete transaction
